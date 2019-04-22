06646c79d2d70ce4346a18976ffedc1c0dc0e30c
Alan Galloway <agalloway@thesoftwareguild.com>
2019-04-22 10:39:18 -0400

    Initial add to The-Software-Guild

---
+++ javascript/1 - Introduction/customJavaScript.js
@@ -0,0 +1,10 @@
+// This is part of the Introduction to Web Development lesson
+// Learn more at:
+// http://thesoftwareguild.ivyapp.com/javascript
+var total = 0;
+var firstNumber = 2;
+var secondNumber = 3;
+ 
+total = firstNumber + secondNumber;
+ 
+console.log(total);
---
+++ javascript/2 - Value Types and Operators/sampleVariables.js
@@ -0,0 +1,8 @@
+var integerNumberVariable = 42;
+var floatNumberVariable = 3.14;
+var stringVariable = "This is a string.";
+var stringVariable2 = 'This is a string too!';
+var stringVariable3 = 'c'; // this is a char in other languages
+var booleanVariable = true;
+var nullVariable = null;
+var undefinedVariable;
